cf562036b43f84e2ab166846193592c451d24a05
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-26 18:41:49 -0400

    Update bot.py

--- Utilites-Discord-Bot/bot.py
+++ Utilites-Discord-Bot/bot.py
@@ -1,1 +1,19 @@
+import discord
+import random
 
+from discord.ext import commands
+
+TOKEN = '<redacted>'
+
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+
+
+bot.run(TOKEN)

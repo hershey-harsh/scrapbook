87c9a9294a25d9cdc0c39f57ae50e5912aec82b3
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-26 18:42:00 -0400

    Update bot.py

--- Utilites-Discord-Bot/bot.py
+++ Utilites-Discord-Bot/bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 TOKEN = '<redacted>'
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -14,6 +14,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-
+@bot.command(name='die_roll', aliases=['roll'])
+async def die_roll(ctx):
+    roll_result = random.randint(1, 6)
+    
+    await ctx.send(f'You rolled a {roll_result}!')
 
 bot.run(TOKEN)

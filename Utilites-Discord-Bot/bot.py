052ee00aea589e8ca898657474af822ce34ed406
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-26 20:57:28 -0400

    Uploaded commands

--- Utilites-Discord-Bot/bot.py
+++ Utilites-Discord-Bot/bot.py
@@ -1,6 +1,5 @@
 import discord
 import random
-
 from discord.ext import commands
 
 TOKEN = '<redacted>'
@@ -17,7 +16,6 @@ async def on_ready():
 @bot.command(name='die_roll', aliases=['roll'])
 async def die_roll(ctx):
     roll_result = random.randint(1, 6)
-    
     await ctx.send(f'You rolled a {roll_result}!')
 
 @bot.command(name='mock')
@@ -31,5 +29,16 @@ async def countdown(ctx, seconds: int):
         await ctx.send(f'{i} seconds remaining...')
         await asyncio.sleep(1)
     await ctx.send('Countdown complete!')
+
+@bot.command(name='reverse')
+async def reverse(ctx, *, text: str):
+    reversed_text = text[::-1]
+    await ctx.send(f'Reversed text: {reversed_text}')
+
+@bot.command(name='random_user')
+async def random_user(ctx):
+    members = ctx.guild.members
+    random_member = random.choice(members)
+    await ctx.send(f'A random user: {random_member.mention}')
 
 bot.run(TOKEN)

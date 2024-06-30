38ab1d76351dc171099fc957351ccb1d257599f6
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-30 06:24:11 -0400

    commit

--- pygame_v1/game.py
+++ pygame_v1/game.py
@@ -15,22 +15,27 @@ class Game:
             'start': Room('Start Room', 'You are in a small room with a wooden door. There is a key here.'),
             'hallway': Room('Hallway', 'You are in a long hallway. There is a door at the end.'),
             'kitchen': Room('Kitchen', 'You are in a kitchen. There is a knife on the table.'),
+            'library': Room('Library', 'You are in a library. There are many books on the shelves.'),
             'treasure': Room('Treasure Room', 'You found the treasure room! There is a chest here.')
         }
         self.rooms['start'].add_exit('north', self.rooms['hallway'])
         self.rooms['hallway'].add_exit('south', self.rooms['start'])
         self.rooms['hallway'].add_exit('east', self.rooms['kitchen'])
+        self.rooms['hallway'].add_exit('west', self.rooms['library'])
         self.rooms['hallway'].add_exit('north', self.rooms['treasure'])
         self.rooms['kitchen'].add_exit('west', self.rooms['hallway'])
+        self.rooms['library'].add_exit('east', self.rooms['hallway'])
         self.rooms['treasure'].add_exit('south', self.rooms['hallway'])
 
         key = Item('key', 'A small rusty key.')
         chest = Item('chest', 'A large wooden chest. It seems to be locked.')
         knife = Item('knife', 'A sharp kitchen knife.')
+        book = Item('book', 'A mysterious book with strange symbols.')
 
         self.rooms['start'].add_item(key)
         self.rooms['treasure'].add_item(chest)
         self.rooms['kitchen'].add_item(knife)
+        self.rooms['library'].add_item(book)
 
     def play(self):
         while self.is_playing:
@@ -66,6 +71,9 @@ class Game:
         elif command.startswith('use '):
             item_name = command.split(' ')[1]
             self.use_item(item_name)
+        elif command.startswith('inspect '):
+            item_name = command.split(' ')[1]
+            self.inspect_item(item_name)
         else:
             print("I don't understand that command.")
 
@@ -77,6 +85,13 @@ class Game:
                 self.is_playing = False
             else:
                 print(f"You can't use the {item.name} here.")
+        else:
+            print(f"You don't have a {item_name}.")
+
+    def inspect_item(self, item_name):
+        item = self.player.get_item(item_name)
+        if item:
+            print(f"Inspecting {item.name}: {item.description}")
         else:
             print(f"You don't have a {item_name}.")
 

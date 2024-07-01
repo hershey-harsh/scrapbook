688872949b3f60a861db67b2b0f8535b802ede4c
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-07-01 15:59:05 -0400

    commit

--- pygamev3/game.py
+++ pygamev3/game.py
@@ -24,13 +24,17 @@ snake_speed = 15
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
+def score_display(score):
+    value = score_font.render("Your Score: " + str(score), True, white)
+    dis.blit(value, [0, 0])
+
 def our_snake(snake_block, snake_List):
     for x in snake_List:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
-def message(msg, color):
+def message(msg, color, y_displace=0):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+    dis.blit(mesg, [dis_width / 6, dis_height / 3 + y_displace])
 
 def gameLoop():
     game_over = False
@@ -48,12 +52,19 @@ def gameLoop():
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
+    score = 0
+
+    pygame.mixer.music.load('background.mp3')
+    pygame.mixer.music.play(-1)
+
     while not game_over:
 
         while game_close == True:
             dis.fill(blue)
             message("You Lost! Press Q-Quit or C-Play Again", red)
+            message(f"Your Score: {score}", white, 50)
             pygame.display.update()
+            pygame.mixer.music.stop()
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
@@ -98,12 +109,16 @@ def gameLoop():
                 game_close = True
 
         our_snake(snake_block, snake_List)
+        score_display(Length_of_snake - 1)
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
+            score += 1
+            if score % 5 == 0:
+                snake_speed += 1
 
         clock.tick(snake_speed)
 
